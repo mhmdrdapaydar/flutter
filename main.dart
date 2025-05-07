3f7d3a87a8ad2637f803728357b4f4536ad19a32
mhmdrdapaydar <79783890+mhmdrdapaydar@users.noreply.github.com>
2025-05-07 21:09:22 +0330

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'todo_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      home: TodoListScreen(),
+    );
+  }
+}
